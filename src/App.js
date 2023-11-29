4db29dd76790d710735a9163f17e7e32f1bfca04
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2023-11-29 19:36:34 +0100

    Analytics hero and newsletter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Analytics from './components/Analytics';
+import NewsLetter from './components/NewsLetter';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <NewsLetter/>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,39 +3,33 @@ import { CiMenuFries } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false)
-
-    const handleNav = () => {
-    setNav(!nav)
-    }
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">React.</h1>
-      <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
       </ul>
-
-      <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <IoMdClose size ={20}/> : <CiMenuFries size={20}/> }
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <IoMdClose size={20}/> : <CiMenuFries size={20} />}
       </div>
-
-        <div className={!nav ? 'fixed left-0 top-0  w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
-        <ul className="uppercase p-4">
-            <li className="p-4 border-b border-gray-600">Home</li>
-            <li className="p-4 border-b border-gray-600">Company</li>
-            <li className="p-4 border-b border-gray-600">Resources</li>
-            <li className="p-4 border-b border-gray-600">About</li>
-            <li className="p-4 border-b border-gray-600">Contact</li>
-        </ul>
-        </div>
-
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
     </div>
   );
 };
